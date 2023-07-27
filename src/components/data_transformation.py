318af335441ee04ff6c90be8ab04818e82ab4815
Vinay <learningfrombirth@gmail.com>
2023-07-28 01:59:54 +0530

    EDA and model trainer

---
+++ src/components/data_transformation.py
@@ -0,0 +1,32 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoderEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        '''
+        This function is responsible for data transformation
+        '''
+        try:
+             # Define which columns should be onehot-encoded and which should be scaled 
+             categorical_cols=['sex','region','smoker']   
+             numerical_cols=['age','bmi','children']
